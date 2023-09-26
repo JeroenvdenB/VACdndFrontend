508cc0f97ba23e829b6ae8afa250912ebf927316
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-09-26 19:28:08 +0200

    second commit

---
+++ config.js
@@ -0,0 +1,70 @@
+//Comment out the local or online url
+var url = "http://localhost:8080/";
+//var url = "https://vacdnd-be.azurewebsites.net/";
+
+var sourceBooks = [
+    "Homebrew",
+    "Acquisitions Incorporated",
+    "Astral Adventurer's Guide",
+    "Baldur's Gate Descent into Avernus",
+    "Boo's Astral Menagerie",
+    "Curse of Strahd",
+    "Domains of Delight",
+    "Dragon Heist",
+    "Dragon of Icespire Peak",
+    "Dragonlance Shadow of the Dragon Queen",
+    "Dungeon Master's Guide",
+    "Dungeon of the Mad Mage",
+    "Eberron Rising from the Last War",
+    "Explorer's Guide to Wildemount",
+    "Fizban's Treasury of Dragon",
+    "Ghosts of Saltmarsh",
+    "Guildmaster's Guide to Ravnica",
+    "Hoard of the Dragon Queen",
+    "Icewind Dale Rime of the Frostmaiden",
+    "Light of Xaryxis",
+    "Lost Laboratory of Kwalish",
+    "Lost Mine of Phandelver",
+    "Minsc and Boo's Journal of Villainy",
+    "Monster Manual",
+    "Monster's of the Multiverse",
+    "Monsterous Compendium",
+    "Mordenkainen's Tome of Foes",
+    "Mythic Odysseys of Theros",
+    "Strixhaven",
+    "Sword Coast Adventurer's Guide",
+    "Tasha's Cauldron of Everything",
+    "Rise of Tiamat",
+    "The Griffon's Saddlebag",
+    "The Wild Beyond the Witchlight",
+    "Tomb of Annihilation",
+    "Tome of Beats",
+    "Van Richten's Guide to Ravenloft",
+    "Volo's Guide to Monsters",
+    "Waterdeep Dragon Heist",
+    "Xanathar's Guide to Everything"
+];
+
+var schoolsOfMagic = [
+    "Abjuration",
+    "Conjuration",
+    "Divination",
+    "Dunamancy",
+    "Enchantment",
+    "Evocation",
+    "Illusion",
+    "Necromancy",
+    "Transmutation"
+]
+
+var spellLists = [
+    "Artificer",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Paladin",
+    "Ranger",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+]
